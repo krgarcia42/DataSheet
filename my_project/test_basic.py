aa0e518b3f4a996dc2e57309d1423089f34f3cbb
krgarcia42 <kyle.g75@yahoo.com>
2026-03-25 11:25:07 -0400

    Update test_basic.py

--- my_project/test_basic.py
+++ my_project/test_basic.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 from ingestor import load_csv_to_db
 
 def test_database_creation():
@@ -8,5 +9,15 @@ def test_database_creation():
         os.remove(db_name)
         
     load_csv_to_db("data.csv", "users", db_name)
-  
+
+    #check if file exists
     assert os.path.exists(db_name) == True
+
+    #check if data is inside file
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users")
+    count = cursor.fetchone()[0]
+    conn.close()
+
+    assert count > 0
