4f7cc036a39ac1495f49587be8a99a2ca16558e5
krgarcia42 <kyle.g75@yahoo.com>
2026-03-30 10:38:08 -0400

    Implement data pipeline with CSV ingestion

--- my_project/main.py
+++ my_project/main.py
@@ -1,1 +1,15 @@
+import schema_manager
+import ingestor
 
+def run_pipeline():
+  csv_file = "data.csv"
+  db_name = "production.db"
+
+  print("begin...")
+
+  create_user_table(db_name)
+
+  ingestor.load_csv_to_db(csv_file, "users", db_name)
+
+if __name__ == "__main__":
+  run_pipeline()
