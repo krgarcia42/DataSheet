338bf1c247818c1d43924ed1fbd33d133d6c4359
krgarcia42 <kyle.g75@yahoo.com>
2026-04-03 09:05:54 -0400

    Refactor get_all_users to use execute_query

--- my_project/query_service.py
+++ my_project/query_service.py
@@ -1,19 +1,19 @@
-import sqlite3
+from database_manager import execute_query
 
-def get_all_users(db_path):
-  conn = sqlite3.connect(db_path)
-  cursor = conn.cursor()
+def get_all_users():
+  query = "SELECT * from users"
 
-  #query to grab everything
-  cursor.execute("SELECT * FROM users")
-  rows = cursor.fetchall()
+  #1. execute query
+  rows = execute_query(query)
 
-  print(f"Found {len(rows)} users")
-  for row in rows:
-    name, age, city = row
-    print(f"User: {name} | Age: {age} | City: {city}")
+  if rows:
+    print(f"--- Query Service: Found {len(rows)} users ---")
+  else:
+    print("--- Query Service: No users found in database ---")
 
-  conn.close()
-
+  #2. return rows to main.py
+  return rows
+  
 if __name__ == "__main__":
-  get_all_users("production.db")
+  data = get_all_users()
+  print(data)
