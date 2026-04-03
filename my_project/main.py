be758b07994cc313788453ebb44f07745f16444d
krgarcia42 <kyle.g75@yahoo.com>
2026-04-03 09:30:03 -0400

    Update main.py

--- my_project/main.py
+++ my_project/main.py
@@ -1,19 +1,25 @@
 from schema_manager import initialize_db
+from ingestor import ingest_data
 from query_service import get_all_users
 from llm_adaptor import ask_gemini_to_summarize
 
 def run_pipeline():
+  #set up SQLite
   print("1. Initializing Database")
   initialize_db()
 
-  print("2. Fetching User Data")
+  #ingest with pandas
+  print("2. Ingesting Data")
+  ingest_data("my_project/data.csv")
+
+  print("3. Fetching User Data")
   users = get_all_users()
 
   if not users:
     print("No users found. Pipeline stopping")
     return
 
-  print("3. Sending to Gemini")
+  print("4. Sending to Gemini")
   summary = ask_gemini_to_summarize(users)
 
   print("Final Summary")
